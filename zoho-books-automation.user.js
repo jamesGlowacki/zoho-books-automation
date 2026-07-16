e88eea2043eeac8f2f3b2a3c028d78c3f7225617
James Glowacki <32254479+jamesGlowacki@users.noreply.github.com>
2026-07-16 16:54:35 -0500

    release v0.2.1

--- zoho-books-automation.user.js
+++ zoho-books-automation.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         Zoho Books Field Automation
 // @namespace    https://github.com/jamesGlowacki/zoho-books-automation
-// @version      0.2.0
+// @version      0.2.1
 // @description  Pre-save client-side field population for Zoho Books. Engine + feature registry, built to grow across pages.
 // @author       James
 // @match        https://books.zoho.com/*
 // @run-at       document-idle
 // @grant        none
-// @updateURL    https://raw.githubusercontent.com/jamesGlowacki/zoho-books-automation/main/zoho-books-automation.user.js
+// @updateURL    https://raw.githubusercontent.com/jamesGlowacki/zoho-books-automation/main/zoho-books-automation.meta.js
 // @downloadURL  https://raw.githubusercontent.com/jamesGlowacki/zoho-books-automation/main/zoho-books-automation.user.js
 // ==/UserScript==
 
